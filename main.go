bc4ff7cf0a72ffc19bb10f462b8e577abc08b9a7
Yongfeng Qin <yongfeng.qin@bukalapak.com>
2022-12-26 21:18:25 +1100

    Add more usage

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 	"time"
 )
 
-func v1Middleware() gee.HandlerFunc {
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func onlyForV2() gee.HandlerFunc {
 	return func(c *gee.Context) {
 		// Start timer
 		t := time.Now()
@@ -17,16 +22,6 @@ func v1Middleware() gee.HandlerFunc {
 		// Calculate resolution time
 		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
 	}
-}
-
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
@@ -39,6 +34,15 @@ func main() {
 		names := []string{"geektutu"}
 		c.String(http.StatusOK, names[100])
 	})
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
 
 	r.Run(":9999")
 }
