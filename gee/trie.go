3a6b76a659263ab0b333358293ef319f07074d91
Yongfeng Qin <yongfeng.qin@bukalapak.com>
2022-12-26 21:10:57 +1100

    Init commit

---
+++ gee/trie.go
@@ -0,0 +1,69 @@
+package gee
+
+import (
+	"log"
+	"strings"
+)
+
+type node struct {
+	pattern  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+func (n *node) insert(parttern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = parttern
+		return
+	}
+	
+	// Check if there is a match in this level, if not create one otherwise use existing one
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(parttern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+	log.Print(part)
+	log.Print(children)
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+	return nil
+}
